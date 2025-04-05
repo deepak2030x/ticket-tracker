a90eea9943aa07a5c4a5b83edc896d4dba3c0aa6
deepak <ardeepak@outlook.com>
2025-04-05 12:41:18 +0530

    style home page and tickets page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import Link from "next/link";
 import { ticketsPath } from "@/paths";
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">Home page</h2>
-      <Link href={ticketsPath()} className="underline">
-        Go to tickets
-      </Link>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="text-sm underline">
+          Go to Tickets
+        </Link>
+      </div>
     </div>
   );
 };
