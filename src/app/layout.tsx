6320da9a530cbd6ea596e2fb5d51f0d407ad7e5f
deepak <ardeepak@outlook.com>
2025-04-06 17:54:04 +0530

    theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
           <div>
             <Link
               href={ticketsPath()}
-              className={buttonVariants({ variant: "outline" })}
+              className={buttonVariants({ variant: "default" })}
             >
               Tickets
             </Link>
