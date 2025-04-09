7fe102d8e25aa1ba352f37043cd11ef464e6217e
deepak <ardeepak@outlook.com>
2025-04-09 16:09:09 +0530

    extract ticket item logic into its own re-usable component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,46 +1,14 @@
-import { LucideCircleCheck, LucideFile, LucidePencil } from "lucide-react";
-import Link from "next/link";
-
 import { Heading } from "@/components/heading";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
+import { TicketItem } from "@/features/ticket/components/ticket-item";
 
-const TICKET_ICONS = {
-  OPEN: <LucideFile />,
-  IN_PROGRESS: <LucidePencil />,
-  DONE: <LucideCircleCheck />,
-};
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" desc="All your tickets at one place" />
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
         {initialTickets.map((ticket) => (
-          <Card key={ticket.id} className="w-full max-w-[420px]">
-            <CardHeader>
-              <CardTitle className="flex gap-x-2">
-                <span>{TICKET_ICONS[ticket.status]}</span>
-                <span className="truncate">{ticket.title}</span>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="line-clamp-3 whitespace-break-spaces">
-                {ticket.content}
-              </p>
-            </CardContent>
-            <CardFooter>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                View
-              </Link>
-            </CardFooter>
-          </Card>
+          <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
     </div>
---
+++ src/features/ticket/constants.tsx
@@ -0,0 +1,7 @@
+import { LucideCircleCheck, LucideFile, LucidePencil } from "lucide-react";
+
+export const TICKET_ICONS = {
+  OPEN: <LucideFile />,
+  IN_PROGRESS: <LucidePencil />,
+  DONE: <LucideCircleCheck />,
+};
