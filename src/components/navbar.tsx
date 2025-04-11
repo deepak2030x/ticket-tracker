8881734aff8a2e213370fdddd98b3f98c54972e3
deepak <ardeepak@outlook.com>
2025-04-11 19:41:54 +0530

    add theme dark-light mode using next-themes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { homePath, ticketsPath } from "@/paths";
 
+import { ThemeSwitcher } from "./theme/theme-switcher";
 import { buttonVariants } from "./ui/button";
 
 const Navbar = () => {
@@ -15,7 +16,7 @@ const Navbar = () => {
             w-full flex py-2.5 px-5 justify-between
           "
     >
-      <div>
+      <div className="flex align-items gap-x-2">
         <Link
           href={homePath()}
           className={buttonVariants({ variant: "ghost" })}
@@ -24,7 +25,8 @@ const Navbar = () => {
           <h1 className="text-lg font-semibold">Ticket_Tracker</h1>
         </Link>
       </div>
-      <div>
+      <div className="flex align-items gap-x-2">
+        <ThemeSwitcher />
         <Link
           href={ticketsPath()}
           className={buttonVariants({ variant: "default" })}
