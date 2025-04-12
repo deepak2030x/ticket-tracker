eadf979080a01fd40c5b384b6268801d4edef8e5
deepak <ardeepak@outlook.com>
2025-04-12 18:29:12 +0530

    add suspense boundary

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,16 +1,17 @@
+import { Suspense } from "react";
+
 import { Heading } from "@/components/heading";
-import { initialTickets } from "@/data";
-import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { Spinner } from "@/components/spinner";
+import { TicketList } from "@/features/ticket/components/ticket-list";
+// import { initialTickets } from "@/data";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" desc="All your tickets at one place" />
-      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {initialTickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket} />
-        ))}
-      </div>
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
